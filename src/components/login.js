1ac193799f47507ef0dc4571f09bd2d3d42c7241
suleimanabubakar <suleiman.u.abubakar@gmail.com>
2021-09-10 14:04:42 +0300

    redux

---
+++ src/components/login.js
@@ -0,0 +1,19 @@
+const Login = () => {
+    return ( 
+        <>
+        <p>Login Page</p>
+        <div>
+            <label htmlFor="#">Email</label>
+            <input type="text" />
+        </div>
+        <div>
+            <label htmlFor="#">Password</label>
+            <input type="text" />
+        </div>
+
+        <button>Login</button>
+        </>
+     );
+}
+ 
+export default Login;
---
+++ src/components/logout.js
@@ -0,0 +1,10 @@
+const Logout = () => {
+    return ( 
+    <>
+    <h1>Welcome Name</h1>
+    <button>Logout</button>
+    </>        
+     );
+}
+ 
+export default Logout;
